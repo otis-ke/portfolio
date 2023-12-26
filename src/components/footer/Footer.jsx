4f16a82d89002743b21a99b0576e4b78892570f1
otis-ke <vincentotiskisia@gmail.com>
2023-12-26 22:43:32 +0300

    must

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './Footer.css'
-import { FaSquareXTwitter } from "react-icons/fa6";
-import { ImGithub } from "react-icons/im";
-import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { BsTwitterX } from "react-icons/bs";
 
 const Footer = () => {
   return (
@@ -22,9 +22,9 @@ const Footer = () => {
       </ul>
       <div className="permalinks"> 
       
-      <a href="https://github.com/otis-ke" target="_blank" rel="noreferrer"><ImGithub /></a>
-           <a href="https://www.linkedin.com/in/vincent-otis-1315b0235" target="_blank" rel="noreferrer"><FaLinkedin /></a>
-           <a href="https://twitter.com/otinaughty" target="_blank" rel="noreferrer"><FaSquareXTwitter  /></a>
+          <a href="https://github.com/otis-ke" target="_blank" rel="noreferrer"><FaGithub/></a>
+           <a href="https://www.linkedin.com/in/vincent-otis-1315b0235" target="_blank" rel="noreferrer"><FaLinkedinIn/></a>
+           <a href="https://twitter.com/otinaughty" target="_blank" rel="noreferrer"><BsTwitterX /></a>
          
       
         
